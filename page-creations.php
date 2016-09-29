37adb002e54fc2bc25c8fbe5b8bc3da1c7c74b09
Matt Hurless <mrhurless@gmail.com>
2016-09-29 21:14:36 +0000

    added a bleh

--- page-creations.php
+++ page-creations.php
@@ -10,7 +10,7 @@ get_header(); ?>
     <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
 
     <div <?php post_class() ?> id="post-<?php the_ID(); ?>">
-        <h2>blah blah<a href="<?php the_permalink() ?>" rel="bookmark" title="Permanent Link to <?php the_title_attribute(); ?>"><?php the_title(); ?></a></h2>
+        <h2>blah blah bleh<a href="<?php the_permalink() ?>" rel="bookmark" title="Permanent Link to <?php the_title_attribute(); ?>"><?php the_title(); ?></a></h2>
         <small><?php the_time('F jS, Y') ?> <!-- by <?php the_author() ?> --></small>
 
         <div class="entry">
